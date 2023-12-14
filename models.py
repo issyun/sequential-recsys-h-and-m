a9ea848f1fd35f3f84ed445b4ddc05f0cb62d7a9
Sihun Lee <sstepoutside@gmail.com>
2023-12-15 04:23:42 +0900

    Fix: Remove second projection layer

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 class GRU4Rec(nn.Module):
     def __init__(self, num_articles, emb_size, hidden_size, num_layers, dropout):
@@ -8,20 +9,18 @@ class GRU4Rec(nn.Module):
         self.num_layers = num_layers
         self.embedding = nn.Embedding(num_articles, emb_size)
         self.gru = nn.GRU(emb_size, hidden_size, num_layers, dropout=dropout, batch_first=True)
-        self.proj_1 = nn.Linear(hidden_size, num_articles//2)
-        self.proj_2 = nn.Linear(num_articles//2, num_articles)
+        self.proj = nn.Linear(hidden_size, num_articles)
         self.init_weight()
         
-    def forward(self, input):
+    def forward(self, input, lengths):
         embedded = self.embedding(input)
-        output, _ = self.gru(embedded)
-        output = self.proj_1(output).relu()
-        output = self.proj_2(output)
+        packed = pack_padded_sequence(embedded, lengths, batch_first=True, enforce_sorted=False)
+        output, _ = self.gru(packed)
+        output, _ = pad_packed_sequence(output, batch_first=True)
+        output = self.proj(output)
         return output
     
     def init_weight(self):
         self.embedding.weight.data.uniform_(-0.1, 0.1)
-        self.proj_1.weight.data.uniform_(-0.1, 0.1)
-        self.proj_1.bias.data.fill_(0)
-        self.proj_2.weight.data.uniform_(-0.1, 0.1)
-        self.proj_2.bias.data.fill_(0)
+        self.proj.weight.data.uniform_(-0.1, 0.1)
+        self.proj.bias.data.fill_(0)

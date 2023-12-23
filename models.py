8e8d6f6446e2ab895325b90268f6b72e7826bfc6
Sihun Lee <sstepoutside@gmail.com>
2023-12-24 07:05:49 +0900

    Add: GRU4Rec w/o user emb, NARM dropout

--- models.py
+++ models.py
@@ -29,6 +29,29 @@ class GRU4Rec(nn.Module):
         self.proj.weight.data.uniform_(-0.1, 0.1)
         self.proj.bias.data.fill_(0)
 
+class GRU4RecNoUser(nn.Module):
+    def __init__(self, num_items, item_emb_size, hidden_size, num_layers, dropout):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(num_items, item_emb_size)
+        self.gru = nn.GRU(item_emb_size, hidden_size, num_layers, dropout=dropout, batch_first=True)
+        self.proj = nn.Linear(hidden_size, num_items)
+        self.init_weight()
+        
+    def forward(self, input, lengths):
+        emb = self.embedding(input)
+        packed = pack_padded_sequence(emb, lengths, batch_first=True, enforce_sorted=False)
+        output, _ = self.gru(packed)
+        output, _ = pad_packed_sequence(output, batch_first=True)
+        output = self.proj(output)
+        return output
+    
+    def init_weight(self):
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        self.proj.weight.data.uniform_(-0.1, 0.1)
+        self.proj.bias.data.fill_(0)
+
 class NARM(nn.Module):
     def __init__(self, num_items, emb_size, hidden_size, num_layers=1):
         super(NARM, self).__init__()
@@ -38,29 +61,30 @@ class NARM(nn.Module):
         self.emb_size = emb_size
 
         self.emb = nn.Embedding(num_items, emb_size)
+        self.emb_do = nn.Dropout(0.25)
         self.gru = nn.GRU(emb_size, hidden_size, num_layers, batch_first=True)
         self.A_1 = nn.Linear(hidden_size, hidden_size, bias=False)
         self.A_2 = nn.Linear(hidden_size, hidden_size, bias=False)
         self.v = nn.Linear(hidden_size, 1, bias=False)
+        self.ct_do = nn.Dropout(0.5)
         self.B = nn.Linear(2 * hidden_size, emb_size, bias=False)
 
     def forward(self, input, lengths):
-        emb = self.emb(input)
+        emb = self.emb_do(self.emb(input))
         packed = pack_padded_sequence(emb, lengths, batch_first=True, enforce_sorted=False)
         gru_out, hidden = self.gru(packed)
         gru_out, _ = pad_packed_sequence(gru_out, batch_first=True)
 
         ht = hidden[-1]
-
         c_global = ht
         q1 = self.A_1(gru_out)
         q2 = self.A_2(ht)
         q2 = q2.unsqueeze(1).expand_as(q1)
-
         alpha = self.v(torch.sigmoid(q1 + q2).reshape(-1, self.hidden_size)).reshape(input.size())
         c_local = torch.sum(alpha.unsqueeze(2).expand_as(gru_out) * gru_out, 1)
 
         c_t = self.B(torch.cat([c_local, c_global], 1)).unsqueeze(2)
+        c_t = self.ct_do(c_t)
         item_embs = self.emb(torch.arange(self.num_items).to(input.device)).unsqueeze(0).expand(input.shape[0], -1, -1)
         scores = torch.bmm(item_embs, c_t).squeeze()
 
